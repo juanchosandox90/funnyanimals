abe55be7d792f313bd4a6013034195dd33cc0811
juan.sandoval <Naruto2790>
2020-03-04 12:21:04 -0500

    feature/001_Navigation  - Navigation Graph  - Fragments to be used  - Actions in navigation

---
+++ app/src/main/java/com/sandoval/funnyanimals/view/fragments/DetailFragment.kt
@@ -0,0 +1,20 @@
+package com.sandoval.funnyanimals.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.sandoval.funnyanimals.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+class DetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
