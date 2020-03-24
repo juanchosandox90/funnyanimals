78c83ab743376c382a66563af2fc982227f0be76
juanchosandox90 <juansandoval222@gmail.com>
2020-03-24 11:31:49 -0500

    feature/006_DataBindingAnimals  - DataBinding enable in gradle  - DataBinging applied in detail and list fragments  - Change adapter implementation to data binding  - Change detail fragment implementation to data binding  - Create data binding function call for onClick  - Create binding adapter to load image via Data Binding

--- app/src/main/java/com/sandoval/funnyanimals/util/Util.kt
+++ app/src/main/java/com/sandoval/funnyanimals/util/Util.kt
@@ -3,6 +3,7 @@ package com.sandoval.funnyanimals.util
 import android.content.Context
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -26,4 +27,9 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
         .load(uri)
         .circleCrop()
         .into(this)
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
 }
